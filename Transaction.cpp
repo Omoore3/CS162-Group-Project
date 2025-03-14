2adaaf191c7b16ca2fa8235489ffa0cf61907c7c
atticus76 <157366256+atticus76@users.noreply.github.com>
2025-03-14 16:30:35 -0700

    Create Transaction.cpp
    Implementation for the Transaction class. Three different possible constructors.

---
+++ Transaction.cpp
@@ -0,0 +1,15 @@
+#include "Transaction.h"
+
+Transaction::Transaction() : amount(0), date(time(0)) {}
+
+Transaction::Transaction(double a) : amount(a), date(time(0)) {}
+
+Transaction::Transaction(double a, time_t d) : amount(a), date(d) {}
+
+double Transaction::getAmount() const {
+	return this->amount;
+}
+
+time_t Transaction::getTime() const {
+	return this->date;
+}
