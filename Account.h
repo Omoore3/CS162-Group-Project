214ba995020dcf2880ad4cb28ba902bb3434535e
atticus76 <157366256+atticus76@users.noreply.github.com>
2025-03-14 16:50:39 -0700

    Create Account.h
    Header file for the pure abstract account class. Every derived class of the base class Account must override the transfer, withdraw, deposit, and get and setBalance() virtual functions.

---
+++ Account.h
@@ -0,0 +1,25 @@
+//Name: Atticus Crook
+//Purpose: Header file for the abstract base class account.
+//Date Modified: 12/02/24
+//Compiler Used: MS VC++ 2022
+
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+#include <vector>
+#include "Transaction.h"
+
+
+class Account {
+private:
+	long account_number;
+	double balance;
+	std::vector<Transaction> transactions;
+public:
+	virtual void transfer(Account *other) = 0;
+	virtual void deposit(double amount) = 0;
+	virtual void withdraw(double amount) = 0;
+	virtual double getBalance() const = 0;
+  virtual void setBalance(double b) = 0;
+};
+
+#endif
