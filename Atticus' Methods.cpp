79afbfbc6e190f97672f4c98429f4ea8b7d308b2
atticus76 <157366256+atticus76@users.noreply.github.com>
2025-03-14 18:31:08 -0700

    Update Atticus' Methods.cpp
    Created the Account class constructors and getter and setter with exceptions for the transactions vector

--- Atticus' Methods.cpp
+++ Atticus' Methods.cpp
@@ -1,5 +1,11 @@
+//Name: Atticus Crook
+//Purpose: Implementation of the functions and constructors of the Transaction and Account class
+//Date Modified: 12/02/24
+//Compiler Used: MS VC++ 2022
+
 #include "Transaction.h"
 #include "Account.h"
+#include <string>
 
 Transaction::Transaction() : amount(0), date(time(0)) {}
 
@@ -14,3 +20,21 @@ double Transaction::getAmount() const {
 time_t Transaction::getTime() const {
 	return this->date;
 }
+
+//function implementation for the account class...
+
+//Possible boounds exception. Function must be passed from inside a
+//try block with the appropriate catch block
+Transaction Account::getTransaction(int i) {
+	if(i >= transactions.size() || i < 0) throw string("Out of bounds error. Aborting process...");
+	return transactions[i];
+}
+
+void Account:setTransaction(int i, Transaction T) {
+	if(i >= transactions.size() || i < 0) throw string("Out of bounds error. Aborting process...");
+	transactions[i] = T;
+}
+
+Account::Account(): account_number(0), balance(0), numOfTransactions(0) {}
+Account::Account(long a, double b): account_number(a), balance(b), numOfTransactions(0) {}
+
