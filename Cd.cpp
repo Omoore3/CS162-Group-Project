27b06f9df9567bcac4d2197bc1847e15c29b8cd3
atticus76 <157366256+atticus76@users.noreply.github.com>
2025-03-19 15:15:49 -0700

    Update and rename cd.cpp to Cd.cpp
    Renamed the Cd implementation file and changed code to used correct class name

--- Cd.cpp
+++ Cd.cpp
@@ -1,32 +1,9 @@
 // CDAccount.cpp
 #include "Cd.h"
 
-// Account class methods
-Account::Account(int id, double balance) : accountId(id), balance(balance) {}
-
-int Account::getAccountId() const {
-    return accountId;
-}
-
-void Account::setAccountId(int id) {
-    accountId = id;
-}
-
-double Account::getBalance() const {
-    return balance;
-}
-
-void Account::setBalance(double balance) {
-    this->balance = balance;
-}
-
-void Account::printAccount() const {
-    cout << "Account ID: " << accountId << " | Balance: $" << fixed << setprecision(2) << balance << endl;
-}
-
 // CDAccount class methods
-CDAccount::CDAccount(int id, double initialDeposit, int termMonths)
-    : Account(id, initialDeposit), termMonths(termMonths) {
+Cd::Cd(int account_number, double balance, int termMonths)
+    : Account(account_number, balance), termMonths(termMonths) {
     interestRate = calculateInterestRate();
     
     time_t currentTime = time(0);
@@ -36,18 +13,18 @@ CDAccount::CDAccount(int id, double initialDeposit, int termMonths)
     time_agreement = mktime(timeStruct);
 }
 
-double CDAccount::calculateInterestRate() const {
+double Cd::calculateInterestRate() const {
     if (termMonths == 3) return 0.025;  // 2.5% for 3 months
     else if (termMonths == 6) return 0.03;  // 3% for 6 months
     else if (termMonths == 12) return 0.05;  // 5% for 12 months
     return 0.0;
 }
 
-bool CDAccount::isMaturityDatePassed() const {
+bool Cd::isMaturityDatePassed() const {
     return difftime(time(0), time_agreement) > 0;
 }
 
-double CDAccount::calculateInterest() {
+double Cd::calculateInterest() {
     if (!isMaturityDatePassed()) {
         throw runtime_error("Cannot calculate interest: Maturity date has not passed yet.");
     }
@@ -57,7 +34,7 @@ double CDAccount::calculateInterest() {
     return interest;
 }
 
-void CDAccount::withdraw(double amount) {
+void Cd::withdraw(double amount) {
     if (!isMaturityDatePassed()) {
         throw runtime_error("Cannot withdraw: Maturity date has not passed yet.");
     }
@@ -67,7 +44,7 @@ void CDAccount::withdraw(double amount) {
     balance -= amount;
 }
 
-void CDAccount::transfer(CDAccount& toAccount, double amount) {
+void Cd::transfer(CDAccount& toAccount, double amount) {
     if (!isMaturityDatePassed()) {
         throw runtime_error("Cannot transfer: Maturity date has not passed yet.");
     }
@@ -78,29 +55,23 @@ void CDAccount::transfer(CDAccount& toAccount, double amount) {
     toAccount.balance += amount;
 }
 
-void CDAccount::printAccount() const {
+void Cd::printAccount() const {
     Account::printAccount();
     cout << "Term: " << termMonths << " months | Interest Rate: "
          << interestRate * 100 << "% | Maturity Date: "
          << put_time(localtime(&time_agreement), "%c") << endl;
 }
 
-// CDAccountSystem class methods
-void CDAccountSystem::addCDAccount(CDAccount account) {
-    cdAccounts.push_back(account);
-    cout << "CD Account added successfully!" << endl;
-}
-
-void CDAccountSystem::printCDAccounts() const {
+void Cd::printCDAccounts() const {
     cout << "\n--- List of CD Accounts ---" << endl;
     for (const auto& account : cdAccounts) {
         account.printAccount();
     }
 }
 
-void CDAccountSystem::transferBetweenAccounts(int fromAccountId, int toAccountId, double amount) {
-    CDAccount* fromAccount = nullptr;
-    CDAccount* toAccount = nullptr;
+void Cd::transferBetweenAccounts(int fromAccountId, int toAccountId, double amount) {
+    Cd* fromAccount = nullptr;
+    Cd* toAccount = nullptr;
 
     for (auto& account : cdAccounts) {
         if (account.getAccountId() == fromAccountId) {
