50205f07d53cc1749b9074e4badaacef9af04079
atticus76 <157366256+atticus76@users.noreply.github.com>
2025-03-19 13:07:08 -0700

    Update Atticus' Methods.cpp
    Included <stdexcept> header. Removed transactions header which is already included in the accounts class.

--- Atticus' Methods.cpp
+++ Atticus' Methods.cpp
@@ -3,8 +3,8 @@
 //Date Modified: 12/02/24
 //Compiler Used: MS VC++ 2022
 
-#include "Transaction.h"
 #include "Account.h"
+#include <stdexcept>
 #include <string>
 
 //Transaction class' methods, getters, setters and constructors
@@ -26,6 +26,10 @@ time_t Transaction::getTime() const {
 //Account class' methods, getters, setters and constructors
 //*********************************************************
 
+
+Account::Account(): account_number(0), balance(0), numOfTransactions(0) {} // default constructor
+Account::Account(long a, double b): account_number(a), balance(b), numOfTransactions(0) {} //constructor for account_number and balance
+
 //Possible boounds exception. Function must be passed from inside a
 //try block with the appropriate catch block
 Transaction Account::getTransaction(int i) {
@@ -38,7 +42,3 @@ void Account::setTransaction(int i, Transaction T) {
 		throw runtime_error("Out of bounds error. Aborting process...");
 	transactions[i] = T;
 }
-
-Account::Account(): account_number(0), balance(0), numOfTransactions(0) {} // default constructor
-Account::Account(long a, double b): account_number(a), balance(b), numOfTransactions(0) {} //constructor for account_number and balance
-
