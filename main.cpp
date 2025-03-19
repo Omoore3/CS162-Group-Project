6f2cc32c38481d34a8a53f7151ad5a27ba3f5bed
atticus76 <157366256+atticus76@users.noreply.github.com>
2025-03-19 14:51:07 -0700

    Update main.cpp
    Created boilerplate for the main file.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include "Bank.h"
 
+using namespace std;
+
+void serialize();
+void deserialize();
+
+int main() {
+  
+  return 0;
+}
+
+void serialize() {
+
+}
+
+void deserialize() {
+
+}
