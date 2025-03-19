fe178613c0a19af6cbfa1d97a0addfdd8df37e91
atticus76 <157366256+atticus76@users.noreply.github.com>
2025-03-19 12:18:31 -0700

    Update and rename savings.h to Savings.h
    Changed class name to match header file name. Capitalized Savings file title. Included Account.h header file as base class for savings class.

---
+++ Savings.h
@@ -0,0 +1,21 @@
+// SavingsAccount.h
+
+#ifndef SAVINGS_H
+#define SAVINGS_H
+
+#include <iostream>
+#include <stdexcept>  // To handle errors
+#inlcude "Account.h"
+
+using namespace std;
+
+// Savings Account class derived from Account class
+class Savings : public Account {
+public:
+    SavingsAccount(int id, double initialDeposit); // Constructor to create a savings account
+
+    void withdraw(double amount) override; // Override withdraw method
+    void printAccount() const override;   // Override to print savings account details
+};
+
+#endif // SAVINGSACCOUNT_H
