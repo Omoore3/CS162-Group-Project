5fe7c410afe41723c5553e0e3d13a7777ffd0db0
atticus76 <157366256+atticus76@users.noreply.github.com>
2025-03-19 12:32:43 -0700

    Update Atticus' Methods.cpp
    Documentation for methods

--- Atticus' Methods.cpp
+++ Atticus' Methods.cpp
@@ -7,11 +7,13 @@
 #include "Account.h"
 #include <string>
 
-Transaction::Transaction() : amount(0), date(time(0)) {}
+//Transaction class' methods, getters, setters and constructors
+//*********************************************************
 
-Transaction::Transaction(double a) : amount(a), date(time(0)) {}
-
-Transaction::Transaction(double a, time_t d) : amount(a), date(d) {}
+Transaction::Transaction() : amount(0), date(time(0)) {} //default constructor
+Transaction::Transaction(double a) : amount(a), date(time(0)) {} //given an amount by default creates a transaction at the time
+								 //the object is instantiated
+Transaction::Transaction(double a, time_t d) : amount(a), date(d) {} //creates a transaction of the amount and time specified
 
 double Transaction::getAmount() const {
 	return this->amount;
@@ -21,20 +23,22 @@ time_t Transaction::getTime() const {
 	return this->date;
 }
 
-//function implementation for the account class...
+//Account class' methods, getters, setters and constructors
+//*********************************************************
 
 //Possible boounds exception. Function must be passed from inside a
 //try block with the appropriate catch block
 Transaction Account::getTransaction(int i) {
-	if(i >= transactions.size() || i < 0) throw runtime_error("Out of bounds error. Aborting process...");
+	if(i >= transactions.size() || i < 0) 
+		throw runtime_error("Out of bounds error. Aborting process...");
 	return transactions[i];
 }
-
 void Account::setTransaction(int i, Transaction T) {
-	if(i >= transactions.size() || i < 0) throw runtime_error("Out of bounds error. Aborting process...");
+	if(i >= transactions.size() || i < 0) 
+		throw runtime_error("Out of bounds error. Aborting process...");
 	transactions[i] = T;
 }
 
-Account::Account(): account_number(0), balance(0), numOfTransactions(0) {}
-Account::Account(long a, double b): account_number(a), balance(b), numOfTransactions(0) {}
+Account::Account(): account_number(0), balance(0), numOfTransactions(0) {} // default constructor
+Account::Account(long a, double b): account_number(a), balance(b), numOfTransactions(0) {} //constructor for account_number and balance
 
