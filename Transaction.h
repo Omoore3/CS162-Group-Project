d2657189c28ce65d1a38192660ac93b9ea2562dc
atticus76 <157366256+atticus76@users.noreply.github.com>
2025-03-19 13:45:04 -0700

    Update Transaction.h
    Set setters to be private to prevent fraud

--- Transaction.h
+++ Transaction.h
@@ -8,6 +8,11 @@ private:
 	time_t date;
 	long otherAccountNumber; //use the account_number from the source account class
 		     //source of 1 represents an external deposit. source of -1 represent a withdrawal
+
+	//Setters which are inaccessible after initial action made. Required by rubric, but inacessible to prevent fraud
+	void setAmount(double a);
+	void setTime(time_t newDate);
+	void setOtherAccountNumber(long newAccout);
 public:
 	//getter methods
 	double getAmount() const;
@@ -18,9 +23,7 @@ public:
 	//In the real world though, publically accessible setter methods should probably
 	//not exist to prevent potential fraud (i.e. rerouting transactions to other accounts.
 	//changing transaction amounts, etc.).
-	void setAmount(double a);
-	void setTime(time_t newDate);
-	void setOtherAccountNumber(long newAccout);
+	
 	Transaction(double a);
 	Transaction(double a, time_t d);
 	Transaction();
