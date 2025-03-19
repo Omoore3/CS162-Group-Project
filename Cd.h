4fad7bf96288333ae9a974494c8e2b1877694ce7
atticus76 <157366256+atticus76@users.noreply.github.com>
2025-03-19 15:11:47 -0700

    Update and rename cd.h to Cd.h
    Renamed header file and include the Account base class header

--- Cd.h
+++ Cd.h
@@ -1,6 +1,8 @@
 // CDAccount.h
-#ifndef CDACCOUNT_H
-#define CDACCOUNT_H
+#ifndef CD_H
+#define CD_H
+
+#include "Account.h"
 
 #include <iostream>
 #include <vector>
@@ -9,25 +11,9 @@
 #include <iomanip>  // For setprecision
 #include <cmath>    // For pow function
 
-using namespace std;
-
-// Account Class (base class)
-class Account {
-protected:
-    int accountId;
-    double balance;
-
-public:
-    Account(int id = 0, double balance = 0.0);
-    int getAccountId() const;
-    void setAccountId(int id);
-    double getBalance() const;
-    void setBalance(double balance);
-    virtual void printAccount() const;
-};
 
 // CDAccount Class (derived from Account)
-class CDAccount : public Account {
+class CD : public Account {
 private:
     time_t time_agreement;  // Maturity date of the CD
     double interestRate;    // Interest rate based on the term
