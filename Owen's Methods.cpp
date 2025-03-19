31481b50d6147d920fbc54ec9a5f3108367f9ff3
yamahaPSS30 <117474575+Omoore3@users.noreply.github.com>
2025-03-19 15:48:50 -0700

    What I got done during the call...

--- Owen's Methods.cpp
+++ Owen's Methods.cpp
@@ -1,10 +1,13 @@
 //Methods primarily for bank and person header files
 #include <iostream>
 #include<algorithm>
+#include<fstream>
 using namespace std;
 //BANK METHODS
 #include "bank.h"
 #include "person.h"
+#include "Account.h"
+#include "Cd.h"
 Bank::Bank() {}
 
 void Bank::addCustomer(string name, int dob, int phone, string email, string pass) {
@@ -35,6 +38,22 @@ void Bank::removeCustomer(int index) {
 	customers[index] = Person();
 }
 
+void Bank::retrieve() {
+	//reads in customer/account data, gets # of transactions, reads in transaction data
+}
+void Bank::store() {
+	//pretty similar to ^
+	ofstream outFile("output.bin", ios::binary);
+	for (int i = 0; i < customers.size(); i++) {
+		outFile.write(customers[i].getName().c_str(), sizeof(customers[i].getName().c_str()));
+		outFile.write(reinterpret_cast<char*>(customers[i].getDOB()), sizeof(customers[i].getDOB()));
+		outFile.write(reinterpret_cast<char*>(customers[i].getPhone()), sizeof(customers[i].getPhone()));
+		outFile.write(customers[i].getEmail().c_str(), sizeof(customers[i].getEmail().c_str()));
+		outFile.write(customers[i].getPass().c_str(), sizeof(customers[i].getPass().c_str()));
+		
+	}
+}
+
 //PERSON METHODS
 Person::Person(string name, int dob, int phone, string email, string pass) { //Consstructor for person with all the parameters
 	this->name = name;
@@ -44,14 +63,29 @@ Person::Person(string name, int dob, int phone, string email, string pass) { //C
 	password = pass;
 }
 
-void Person::createAccount(){
+void Person::createAccount(long num, double balance, int type) {
 	//Call account constructor, based on the number given
+	if (type == 1) {//Calls CD constructor
+		int term;
+		cout << "How many months is the term?" << endl;
+		cin >> term;
+		Cd myCd(num, balance, term);
+	}
+	else if (type == 2) {//Calls MM constructor
+
+	}
+	else if (type == 3) {//Calls Savings constructor
+
+	}
+	else if (type == 4) {//Calls Checking constructor
+
+	}
 }
 Account Person::getAccount(int index) {
 	return accounts[index]; //Returns the account at the given index
 }
 void Person::removeAccount(int index) {
-	~Account[index]; //Calls the deconstructor for the account linked to that index
+	~Account accounts[index]; //Calls the deconstructor for the account linked to that index
 }
 Person::Person() { //Default constructor that sets all customer identifiers to null, as a way to erase customers.
 	name = '\0';
