eb819b0b972f2747feb3c338628ea5588bd888bd
atticus76 <157366256+atticus76@users.noreply.github.com>
2025-03-20 09:02:05 -0700

    Update and rename checkings.h to Checking.h

--- Checking.h
+++ Checking.h
@@ -1,32 +1,28 @@
-//CheckingAccount.h
+//Checking.h
 
-#indef CHECKINGACCOUNT_H
-#define CHECKINGACCOUNT_H
+#indef CHECKING_H
+#define CHECKING_H
 #include <iostream>
 
-class CheckingAccount {
+class Checking {
 private: 
     long cardNo;      // Card Number
     int securityCode; // Security code for the card
     int pin;          // Card pin
-    double balance;   // Account's balance
 
 public: 
       // Contructor to initalize the checking account with card information and balance
-      CheckingAccount(long card, int secCode, int pinNumber, double initialBalance);
+      Checking(long card, int secCode, int pinNumber, double balance);
 
     // Methods to perform actions on the account
       void withdraw(double amount); // Withdraw money from the account
       void deposit(double amount);  // Deposit money from the account
       void transfer(double amount); // Transfer money from the account
 
-    // Receiving the balance of the account
-      double getBalance() const; 
-
     // Way to validate pin
       bool pinError(int enteredPin) const;
       void printAccountInfo() const;
 };
 
-     #endif // CHECKINGACCOUNT_H
+ #endif // CHECKING_H
 
