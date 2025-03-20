0ddacd4407ace18a4dd8a44fe4476c8a8dab2b0b
Coder-droid-cloud <oscarorojas06@gmail.com>
2025-03-20 12:49:50 -0700

    moneymarket.h

--- moneymarket.h
+++ moneymarket.h
@@ -1,47 +1,22 @@
-// MoneyMarketAccount.h
-#ifndef MONEYMARKETACCOUNT_H
-#define MONEYMARKETACCOUNT_H
+#ifndef MONEYMARKET_H
+#define MONEYMARKET_H
 
+#include <string>
 #include <iostream>
-#include <stdexcept>
-#include <iomanip>
-#include <ctime>
-#include <cmath>
 
 using namespace std;
 
-// Base Account Class
-class Account {
-protected:
-    int accountId;
+class MoneyMarket {
+private:
     double balance;
     double interestRate;
 
 public:
-    Account(int id = 0, double balance = 0.0, double rate = 0.0);
-    int getAccountId() const;
-    void setAccountId(int id);
-    double getBalance() const;
-    void setBalance(double balance);
-    virtual void deposit(double amount);
-    virtual void transfer(Account& toAccount, double amount);
-    virtual void withdraw(double amount);
-    virtual void printAccount() const;
-    virtual double calculateInterest() const = 0;  // To be implemented by derived classes
+    MoneyMarket(double balance, double interestRate);
+    void deposit(double amount);
+    void withdraw(double amount);
+    void calculateInterest();
+    void displayBalance() const;
 };
 
-// MoneyMarketAccount Class (derived from Account)
-class MoneyMarketAccount : public Account {
-private:
-    static constexpr double feeThreshold = 10000.00;  // Balance threshold to avoid fees
-
-public:
-    MoneyMarketAccount(int id, double initialDeposit);
-    void deposit(double amount) override;
-    void transfer(Account& toAccount, double amount) override;
-    void withdraw(double amount) override;
-    double calculateInterest() const override;
-    void printAccount() const override;
-};
-
-#endif // MONEYMARKETACCOUNT_H
+#endif
