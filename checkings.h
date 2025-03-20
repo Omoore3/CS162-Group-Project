b9cbfc248199d49cfb7d25bcfd69c692aacd0142
Coder-droid-cloud <oscarorojas06@gmail.com>
2025-03-20 12:49:27 -0700

    checkings.h

--- checkings.h
+++ checkings.h
@@ -1,33 +1,20 @@
-//CheckingAccount.h
+#ifndef CHECKING_H
+#define CHECKING_H
 
-#indef CHECKINGS_H
-#define CHECKINGS_H
+#include <string>
 #include <iostream>
-using namespace std; 
 
-class CheckingAccount {
-private: 
-    long cardNo;      // Card Number
-    int securityCode; // Security code for the card
-    int pin;          // Card pin
-    double balance;   // Account's balance
+using namespace std;
 
-public: 
-      // Contructor to initalize the checking account with card information and balance
-      CheckingAccount(long card, int secCode, int pinNumber, double initialBalance);
+class Checking {
+private:
+    double balance;
 
-    // Methods to perform actions on the account
-      void withdraw(double amount); // Withdraw money from the account
-      void deposit(double amount);  // Deposit money from the account
-      void transfer(double amount); // Transfer money from the account
-
-    // Receiving the balance of the account
-      double getBalance() const; 
-
-    // Way to validate pin
-      bool pinError(int enteredPin) const;
-      void printAccountInfo() const;
+public:
+    Checking(double balance);
+    void deposit(double amount);
+    void withdraw(double amount);
+    void displayBalance() const;
 };
 
-     #endif // CHECKINGS_H
-
+#endif
