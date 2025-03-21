5cbd231c56336a37b05dc7617716e324a65093dd
yamahaPSS30 <117474575+Omoore3@users.noreply.github.com>
2025-03-20 19:33:42 -0700

    New header files just dropped
    Aristotle's been quiet since this one

--- bank.h
+++ bank.h
@@ -1,19 +1,15 @@
-#ifndef BANK_H
-#define BANK_H
 #include <vector>
 #include "person.h"
 using namespace std;
 class Bank {
 private:
 	vector<Person> customers;
-protected:
-	double const MM_INTEREST = 0.0125;
-	double const CHECK_INTEREST = 0.0002;
 public:
 	Bank();
 	void addCustomer(string name, int dob, int phone, string email, string pass);
 	int findCustomerIndex(Person customer);
 	void removeCustomer(int index);
 	Person getCustomer(int index);
+	void store();
+	void retrieve();
 };
-#endif
--- person.h
+++ person.h
@@ -7,15 +7,16 @@ private:
 	int phoneNo;
 	string email;
 	string password;
-	Account accounts[4]; //each account will have an assigned number: 0 for CD, 1 for Money Market, etc. TO BE DEFINED
+	Account* accPtr[4]; //each account will have an assigned number: 0 for CD, 1 for Money Market, etc. TO BE DEFINED
 protected:
 	void setPass(string pass) { password = pass; }
 	void setDOB(int DOB) { dateOfBirth = DOB; }
 public:
 	Person(string name, int dob, int phone, string email, string pass);
 	Person();
-	void createAccount();
-	Account getAccount(int index);
+	void createAccount(long num, double balance, int type);
+	
+	Account* getAccount(int index);
 	void removeAccount(int index);
 	//No deconstructor is needed, since the only objects getting added deal with being in a container, needing erase()
 	
