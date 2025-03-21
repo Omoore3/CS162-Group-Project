665a0992e93d85e1d9aa2869b0357ab601a592c2
atticus76 <157366256+atticus76@users.noreply.github.com>
2025-03-20 19:23:23 -0700

    Update CD.h
    Formatting

--- CD.h
+++ CD.h
@@ -12,23 +12,22 @@ using namespace std;
 // CDAccount Class (derived from Account)
 class CD : public Account {
 private:
-    time_t time_agreement;  // Maturity date of the CD
-    int termMonths;         // The term length of the CD in months
-    bool maturityReached;
+	time_t time_agreement;  // Maturity date of the CD
+	int termMonths;         // The term length of the CD in months
+	bool maturityReached;
 
-    static const double CD_3_MON = 0.025;
+	static const double CD_3_MON = 0.025;
 	static const double CD_6_MON = 0.03;
 	static const double CD_12_MON = 0.05;
-
-    double calculateInterestRate() const;
+    	double calculateInterestRate() const;
 public:
-    CD(long account_number, double balance, int termMonths);
-    bool isMaturityDatePassed() const;
-    double calculateInterest();
-    void deposit(double amount) override final;  // Disable deposit function for this class
-    void withdraw(double amount) override final;
-    void transfer(double amount, Account* recipient) override final;
-    void printAccount() const;
+	CD(long account_number, double balance, int termMonths);
+	bool isMaturityDatePassed() const;
+	double calculateInterest();
+    	void deposit(double amount) override final;  // Disable deposit function for this class
+    	void withdraw(double amount) override final;
+    	void transfer(double amount, Account* recipient) override final;
+    	void printAccount() const;
 };
 
 #endif
