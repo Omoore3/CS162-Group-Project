4f2216e82d630fa66f705f555544adf3b6d2113b
atticus76 <157366256+atticus76@users.noreply.github.com>
2025-03-20 18:54:51 -0700

    Update Account.h
    Removed lines related to the no longer implemented transaction class

--- Account.h
+++ Account.h
@@ -6,7 +6,6 @@
 #ifndef ACCOUNT_H
 #define ACCOUNT_H
 #include <vector>
-#include "Transaction.h"
 
 using namespace std;
 
@@ -14,24 +13,18 @@ class Account {
 private:
 	long account_number; //unique identifier for each account class. Need a method
 	double balance;	     //to verrify that account_number is unique in all classes (Use a map?)
-	vector<Transaction> transactions;
-	int numOfTransactions;
 
 	//setters which should be inaccessible to prevent fraud, but are required for the rubric
-	void setTransaction(int i, Transaction T);
 	void setBalance(double balance);
 	void setAccountNumber(long account_number);
-	void setNumOfTransactions();
 protected:
 	//some of the virtual functions to be inherited by subclasses
 	virtual void transfer(double amount, Account* recipient) = 0;
 	virtual void deposit(double amount) = 0;
 	virtual void withdraw(double amount) = 0;
 	//getters
-	Transaction getTransaction(int i) const;
 	double getBalance() const;
 	long getAccountNumber() const;
-	int getNumOfTransactions() const;
 	//Constructors
 	Account();
 	Account(long account_number, double balance);
