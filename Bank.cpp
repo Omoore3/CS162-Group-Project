a4841c28129ece8d480a9427c93f64bc42c18151
yamahaPSS30 <117474575+Omoore3@users.noreply.github.com>
2025-03-20 19:09:43 -0700

    Added bank.cpp
    Split and updated from Owen's methods

---
+++ Bank.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include<algorithm>
+#include<fstream>
+using namespace std;
+//BANK METHODS
+#include "bank.h"
+#include "person.h"
+#include "Account.h"
+#include "Cd.h"
+Bank::Bank() {}
+
+void Bank::addCustomer(string name, int dob, int phone, string email, string pass) {
+	//if there is no voided(removed customers) adds new customer to the back of the vector. Else, it replaces the first voided customer to save memory.
+	for (int i = 0; i < customers.size(); i++) {
+		if (customers[i].getPhone() == NULL) {
+			customers[i] = Person(name, dob, phone, email, pass);
+			return;
+		}
+		customers.emplace_back(name, dob, phone, email, pass);
+	}
+}
+int Bank::findCustomerIndex(Person customer) {//Searches for a customer by their phone number, since that will always be unique.
+	for (int i = 0; i < customers.size(); i++) {
+		if (customers[i].getPhone() == customer.getPhone()) {
+			return i;
+		}
+		cout << "Customer not found" << endl;
+		return -1;
+	}
+}
+
+Person Bank::getCustomer(int index) {
+	return customers[index];
+}
+
+void Bank::removeCustomer(int index) {
+	customers[index] = Person();
+}
+
+void Bank::retrieve() {
+	//reads in customer/account data; Assuming that all objects are already created.
+	ifstream inFile("output.bin", ios::binary);
+	int readsize;
+	inFile.read(reinterpret_cast<char*>(&readsize), sizeof(readsize));
+
+
+	for (int i = 0; i < readsize; i++) {
+
+	}
+}
+void Bank::store() {
+	//pretty similar to ^
+	ofstream outFile("output.bin", ios::binary);
+	outFile.write(reinterpret_cast<char*>(customers.size()), sizeof(customers.size());
+
+	for (int i = 0; i < customers.size(); i++) {
+		outFile.write(customers[i].getName().c_str(), sizeof(customers[i].getName().c_str())); //Saving the 
+		outFile.write(reinterpret_cast<char*>(customers[i].getDOB()), sizeof(customers[i].getDOB()));
+		outFile.write(reinterpret_cast<char*>(customers[i].getPhone()), sizeof(customers[i].getPhone()));
+		outFile.write(customers[i].getEmail().c_str(), sizeof(customers[i].getEmail().c_str()));
+		outFile.write(customers[i].getPass().c_str(), sizeof(customers[i].getPass().c_str()));
+
+		if (customers[i].getAccount<CD>(0).getNumber() != 0) {//DEFINE getNumber OR SOMETHING OF SIMILAR FUNCTION THAT GETS THE ACCOUNT NUMBER
+			outFile.write(reinterpret_cast<char*>(customers[i].getAccount<CD>(0)), sizeof(customers[i].getAccount<CD>(0)));
+		}
+		if (customers[i].getAccount<MM>(1).getNumber() != 0) {
+			outFile.write(reinterpret_cast<char*>(customers[i].getAccount<MM>(1)), sizeof(customers[i].getAccount<MM>(1)));
+		}
+		if (customers[i].getAccount<Savings>(2).getNumber() != 0) {
+			outFile.write(reinterpret_cast<char*>(customers[i].getAccount<Savings>(2)), sizeof(customers[i].getAccount < Savings(2)));
+		}
+		if (customers[i].getAccount<Checking>(3).getNumber() != 0) {
+			outFile.write(reinterpret_cast<char*>(customers[i].getAccount<Checking>(3)), sizeof(customers[i].getAccount<Checking>(3)));
+		}
+	}
+	outFile.close();
+}
